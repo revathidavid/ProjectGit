d35a08d72f5873f760ec1388683df085e3d1ec6c
revathidavid <revathi09david@gmail.com>
2021-12-09 17:50:46 +0530

    The Task is Done

---
+++ src/org/sample/sample.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class sample {
+	
+		public void add() {
+			System.out.println("add");
+		}
+
+		public static void main(String[] args) {
+			sample g = new sample();
+			g.add();
+		}
+	}
+
+
